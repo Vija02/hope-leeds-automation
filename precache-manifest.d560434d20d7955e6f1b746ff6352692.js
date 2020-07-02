a65af51527500420dcbe4d67fa51d5facdce8fd7
Michael Salim <salim_michaelmi@yahoo.com>
2020-07-02 22:25:03 +0100

    Updates

---
+++ precache-manifest.d560434d20d7955e6f1b746ff6352692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03321a3b73a4bd57dd2e73b0c18ac11a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7749f3d9273e38faebe",
+    "url": "/static/css/main.fa4c7be9.chunk.css"
+  },
+  {
+    "revision": "55137b7ed7dc037bcddc",
+    "url": "/static/js/2.b8ca01d4.chunk.js"
+  },
+  {
+    "revision": "8b5f34ee704968dec0c517b3d5f76a4f",
+    "url": "/static/js/2.b8ca01d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7749f3d9273e38faebe",
+    "url": "/static/js/main.0973c93a.chunk.js"
+  },
+  {
+    "revision": "5ed25ab2e4dfb5226b91",
+    "url": "/static/js/runtime-main.dcb50b3f.js"
+  }
+]);
