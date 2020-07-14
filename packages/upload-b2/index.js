c54e9714de72a98b8bfc9863b9d6d9109dded9a4
Michael Salim <salim_michaelmi@yahoo.com>
2020-07-14 15:02:52 +0100

    feat: enforce https in frontend

--- packages/upload-b2/index.js
+++ packages/upload-b2/index.js
@@ -2,6 +2,13 @@ const Uppy = require('@uppy/core')
 const Dashboard = require('@uppy/dashboard')
 const Tus = require('@uppy/tus')
 
+// DEBT: With our host, it's much easier to handle this here.
+// Since Github pages & Heroku both allows either protocol
+// Let's just enforce it https here, and also let the backend env to use https
+if (location.protocol !== 'https:') {
+	location.replace(`https:${location.href.substring(location.protocol.length)}`)
+}
+
 var uppy = Uppy()
 	.use(Dashboard, {
 		inline: true,
